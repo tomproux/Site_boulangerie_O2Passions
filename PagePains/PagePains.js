e0ee36e5e47447b17a9233685ba19b6dc8fdb028
tomproux <tomproux53@gmail.com>
2026-06-22 19:10:15 +0200

    add severals pages

--- PagePains/PagePains.js
+++ PagePains/PagePains.js
@@ -2,7 +2,7 @@
 const pageData = {
   title: 'Nos pains',
   backLink: {
-    href: '../../O2Passions.html',
+    href: '../O2Passions.html',
     label: 'Retourner à la page précédente'
   },
   heroImage: {
